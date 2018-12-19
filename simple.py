2c9c1d394c39c403e44be36720681f66405d83e3
Jackson Warren <jackson-jackson@users.noreply.github.com>
2018-12-19 09:36:03 -0800

    adjust format description

--- simple.py
+++ simple.py
@@ -34,7 +34,7 @@ def post_listing_to_slack(sc, listing):
     :param sc: A slack client.
     :param listing: A record of the listing.
     """
-    desc = '{0} | {1} | {2}'.format(listing['price'], listing['name'], listing['url'])
+    desc = '| {0} | {1} | {2}'.format(listing['price'], listing['name'], listing['url'])
     sc.api_call(
         'chat.postMessage', channel=SLACK_CHANNEL, text=desc,
         username='pybot', icon_emoji=':robot_face:'
